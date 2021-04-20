1f40d7cd539e2c241d74927effdeb47c3b55684d
ShimizuKazuki <shimizu@posl.ait.kyushu-u.ac.jp>
2021-04-20 17:43:23 +0900

    Connect to database

--- database/migrations/2021_04_20_083510_create_books_table.php
+++ database/migrations/2021_04_20_083510_create_books_table.php
@@ -15,6 +15,7 @@ class CreateBooksTable extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title');
             $table->timestamps();
         });
     }
